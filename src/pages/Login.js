ed772ac83f2c81eb878d663ff28dba245050108a
sws1552 <sws1552@gmail.com>
2022-02-11 18:37:28 +0900

    Revert "Revert "index.js 스토어 주입 및 로그인/회원가입 페이지 생성""
    This reverts commit 3bb804a207bc0afb35907575d7b359ad222bbfaa.

---
+++ src/pages/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, Input, Grid, Button } from "../elements";
+
+import {useDispatch} from "react-redux"
+import {actionCreators as userActions} from "../redux/modules/user"
+
+const Login = (props) => {
+    const dispatch = useDispatch();
+
+    return (
+        <React.Fragment>
+            <Grid>
+
+            </Grid>
+        </React.Fragment>
+
+    )
+
+
+}
+
+
+export default Login;
