3bb804a207bc0afb35907575d7b359ad222bbfaa
sws1552 <sws1552@gmail.com>
2022-02-11 18:37:03 +0900

    Revert "index.js 스토어 주입 및 로그인/회원가입 페이지 생성"
    This reverts commit 30839f6633a390027206aebdf1d64f9ada0654ca.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './shared/App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-
-import store from "./redux/configureStore"
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
