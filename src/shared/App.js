8cf2e7533d80b0b1282844b1ad94e099383772cc
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-02-11 20:26:33 +0900

    backgroundColor

--- src/shared/App.js
+++ src/shared/App.js
@@ -7,20 +7,30 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <Container>
-          메인페이지
-      </Container>
+      <Back>
+        <Container>메인페이지</Container>
+      </Back>
     </React.Fragment>
   );
 }
+
+const Back = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #eee;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+
+`
 
 const Container = styled.div`
   width: 80vw;
   height: 80vh;
   margin: auto;
-  background-color: orange;
-  border 1px solid #ddd;
-  border-radius: 10px;
+  // background-color: orange;
+  // border 1px solid #ddd;
+  // border-radius: 10px;
   // padding: 60px 20px 130px;
 
 `
