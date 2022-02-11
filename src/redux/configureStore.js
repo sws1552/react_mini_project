8c521c26c34f47bce57e3b67f276a62deaa6c2c6
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-02-11 17:59:49 +0900

    폴더 추가 및 리덕스 내용 추가

---
+++ src/redux/configureStore.js
@@ -0,0 +1,53 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+
+import User from "./modules/user";
+
+export const history = createBrowserHistory();
+
+
+const rootReducer = combineReducers({
+  user: User,
+
+  // 만든 history랑 라우터를 연결시켜 줌
+  router: connectRouter(history),
+});
+
+// withExtraArgument - 다른 인수를 더 넘겨줄게~ => 히스토리를 만든 히스토리로 넘겨주기
+const middlewares = [thunk.withExtraArgument({history:history})];
+
+// 지금이 어느 환경인 지 알려줘요. (개발환경, 프로덕션(배포)환경 ...)
+const env = process.env.NODE_ENV;
+
+// 개발환경에서는 로거라는 걸 하나만 더 써볼게요.
+// 개발할 때만 필요하기 때문에 import 안하고 require로 불러오는거! 다른 때는 굳이 두면 모듈 크기만 커짐
+if (env === "development") {
+  // require는 패키지 쓰려고 가지고 올 때 사용
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+// 안 써도 상관은 없지만 쓰면 엄청 편함!
+const composeEnhancers =
+  // 브라우저 환경일 때만 돌아가도록 함
+  // window redux devtools가 깔려있으면 열어준다는 뜻
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+// 미들웨어 묶기
+// 지금까지 있었던 모든 미들웨어 합치기
+const enhancer = composeEnhancers(
+    applyMiddleware(...middlewares)
+    );
+
+// rootReducer랑 enhancer 합침
+let store = (initialStore) => createStore(rootReducer, enhancer);
+
+export default store();
+
+
---
+++ src/redux/modules/user.js
@@ -0,0 +1,44 @@
+import {createAction, handleActions} from "redux-actions";
+import {produce} from "immer";
+
+// 액션타입
+const SET_USER = "SET_USER";
+
+// 액션생성함수
+const setUser = createAction(SET_USER, (user) => ({user}))
+
+// 초기값
+const initialState = {
+    userID: "iamuser",
+    nickname: "닉네임",
+    password: "12345678",
+    passwordCheck: "12345678"
+};
+
+
+// 미들웨어 자리
+
+
+
+// 리듀서
+export default handleActions({
+    [SET_USER]: (state, action) => produce(state, (draft)=>{
+        // "is_login" : 함수이름, "success" : 저장할 값 
+        // setCookie("is_login", "success")
+        // action creators에서 받아온 값
+        draft.user = action.payload.user;
+        // draft.is_login = true;
+    }),
+
+    initialState
+
+})
+
+
+// action creator export
+const actionCreators = {
+    setUser
+
+};
+
+export { actionCreators }
