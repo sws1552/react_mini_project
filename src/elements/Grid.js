4b0981b5c2aef3c86115fb0e7db6e23b559bcaba
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-02-12 14:11:12 +0900

    Grid 충돌 수정

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -42,10 +42,10 @@ const GridBox = styled.div`
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
   ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
   ${(props) =>
-    props.is_flex
-      ? `display: flex; align-items: center; justify-content: space-between; `
-      : ""}
+    (props.is_flex
+      ? `display: flex; align-items: center; justify-content: space-between;`: "")}
   ${(props) => (props.center ? 'text-align: center' : '')}
-  ${(props) => (props._wrap ? 'flex-flow: wrap' : '')} 
+  ${(props) => (props._wrap ? 'flex-flow: wrap' : '')}
 
+  
 export default Grid;
