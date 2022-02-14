eb159a7f75499cf3407874b8a22f00320976f7e3
sws1552 <sws1552@gmail.com>
2022-02-14 09:09:12 +0900

    추가페이지 수정중

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -4,7 +4,7 @@ import isNull from "redux-actions/lib/utils/isNull";
 import styled from "styled-components";
 
 const Grid = (props) => {
-  const { is_flex, width, margin, padding, bg, children, center, _wrap, tag_flex,} = props;
+  const { is_flex, width, margin, padding, bg, children, center, _wrap,} = props;
 
 
   const styles = {
@@ -15,7 +15,6 @@ const Grid = (props) => {
       bg: bg,
       center: center,
       _wrap: _wrap,
-      tag_flex: tag_flex,
   };
   return (
     <React.Fragment>
@@ -33,7 +32,6 @@ Grid.defaultProps = {
   bg: false,
   center: false,
   _wrap: null,
-  tag_flex: false,
 };
 
 const GridBox = styled.div`
@@ -48,7 +46,6 @@ const GridBox = styled.div`
       ? `display: flex; align-items: center; justify-content: space-between;`: "")}
   ${(props) => (props.center ? `text-align: center` : '')}
   ${(props) => (props._wrap ? `flex-flow: wrap` : '')}
-  ${(props) => (props.tag_flex ? `display: flex` : "" )}
   
 `
   
--- src/pages/PostWrite.js
+++ src/pages/PostWrite.js
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
 import styled from "styled-components";
-
+import {MdDeleteForever} from "react-icons/md";
 
 
 import {Button, Grid, Image, Input, Text} from "../elements";
@@ -13,13 +13,34 @@ const PostWrite = (props) => {
 
     const [tagData, setData] = React.useState([]);
 
+    const tagRef = useRef();
+
+    const [count, setCount] = React.useState(0);
+
+    const input_count = Array.from({length: count}, (v, i) => i );
+
     const tagPlus = () => {
-        
+        if(count < 5){
+            setCount(count+1);
+        }
     }
 
+    const tagAdd = (e) => {
+        if(e.keyCode === 13){
+            e.target.disabled = true;
+            e.target.style.backgroundColor = "black";
+            e.target.style.color = "white";
+            setData([...tagData, e.target.value]);
+        }
+    }
+
+    const tagDelete = (e) => {
+        console.log(tagRef.current.value);
+    } 
 
     const postBtn = () => {
         console.log("버튼 클릭!");
+        console.log("tagData !! ", tagData);
     }
     
     return (
@@ -43,7 +64,28 @@ const PostWrite = (props) => {
                 <Text >최소 1개 이상의 태그를 작성해주세요 (최대 5개) </Text>
                 <Grid>
                     <Button text="태그 추가" width="15%" _onClick={tagPlus}></Button>
-                    <TagInput />
+                        {input_count.map((item, i) => {
+                            return (
+                                <TagDiv key={i}>
+                                    <TagInput key={i+1} placeholder="입력후 Enter" onKeyUp={tagAdd} ref={tagRef} id={i}/>
+                                    <span
+                                    key={i+2} 
+                                    onClick={tagDelete}
+                                    style={{
+                                        fontSize:"2rem",
+                                        color:"red",
+                                        position:"absolute",
+                                        bottom:0,
+                                        left:"5px",
+                                        cursor: "pointer",
+                                    }}>
+                                    <MdDeleteForever                       
+                                    />
+                                    </span>
+                                        
+                                </TagDiv>
+                            )
+                        })}
                 </Grid>
             </Grid>
 
@@ -55,11 +97,20 @@ const PostWrite = (props) => {
 }
 
 const TagInput = styled.input`
-    padding: 10px 0;
+    padding: 10px 3px;
     box-sizing: border-box;
     border: 1px solid black;
     margin-left: 10px;
     width: 15%;
+    text-align: center;
 `;
+
+const TagDiv = styled.div`
+    display: inline;
+    position: relative;
+`;
+
+
+
 
 export default PostWrite;
